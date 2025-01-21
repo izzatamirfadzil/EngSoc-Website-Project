227884371f98a96405b45fb94fb752228ecd81b4
Izzat Amir <izzatamirfadzil@gmail.com>
2025-01-21 17:21:22 +0800

    Adding contents to committee page

--- script.js
+++ script.js
@@ -21,9 +21,3 @@ document.querySelector('.cta-button').addEventListener('click', function (e) {
     });
 });
 
-const mobileMenu = document.querySelector('.mobile-menu');
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    mobileMenu.classList.toggle('active');
-});
