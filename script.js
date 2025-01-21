b7990056e3046493e87c69c9ed5d4970d5793895
Izzat Amir <izzatamirfadzil@gmail.com>
2025-01-21 16:01:08 +0800

    adding footer to every page

--- script.js
+++ script.js
@@ -21,3 +21,9 @@ document.querySelector('.cta-button').addEventListener('click', function (e) {
     });
 });
 
+const mobileMenu = document.querySelector('.mobile-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+});
